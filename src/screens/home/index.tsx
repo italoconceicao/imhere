c0f1fe6a4a5b5bf24ea9262a2861710c5eb8b6c9
Italo Conceição <italooliveiradaconceicao@gmail.com>
2024-08-28 20:31:41 -0300

    primeiro commit

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, TextInput, View, } from "react-native";
+
+import { styles } from './styles'
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.eventName}>Nome do Evento</Text>
+            <Text style={styles.eventDate}>Quarta-feira, 28 de Agosto de 2024</Text>
+
+            <TextInput
+                style={styles.input}
+                placeholder="Nome do participante"
+                placeholderTextColor='#6B6B6B' />
+        </View>
+    )
+}
